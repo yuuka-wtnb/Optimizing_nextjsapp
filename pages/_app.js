950d5aface5c0478480b5e34147e140eeae30aed
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-07-26 23:19:14 +0900

    Working with the _app.js File

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Head from "next/head";
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta
+          name="viewport"
+          content="initial-scales=1.0, width=device-width"
+        ></meta>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
